c50bd32e756bf256972bde09833967228b78fc92
Marlon Brito dos Santos Castor <148072815+Marlon-Brito@users.noreply.github.com>
2023-11-23 19:31:22 -0300

    Initial commit

---
+++ hello.js
@@ -0,0 +1,10 @@
+console.log('Olá, mundo!');
+
+/* Comandos:
+    npm -v = mostra a versão do npm
+    npm ls = exibe todos os pacotes instalados no projeto
+    cls = limpa a tela
+    npm init = inicializa o gerenciador instalando o package.json, arquivo que descreve os pacotes e dependências necessários para o projeto
+
+    JSON: modelo de arquivo usado para armazenamento e transmissão de dados no formato texto, muito utilizado para estruturar informações
+*/
