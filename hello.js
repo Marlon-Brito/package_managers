f71fc74e320a30d14abc4110d1f5f58f05b2dad6
Marlon Brito dos Santos Castor <148072815+Marlon-Brito@users.noreply.github.com>
2023-11-23 20:00:24 -0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -5,6 +5,7 @@ console.log('Olá, mundo!');
     npm ls = exibe todos os pacotes instalados no projeto
     cls = limpa a tela
     npm init = inicializa o gerenciador instalando o package.json, arquivo que descreve os pacotes e dependências necessários para o projeto
-
+    
+    package.json : gerencia os pacotes, documenta o projeto e automaiza alguns serviços
     JSON: modelo de arquivo usado para armazenamento e transmissão de dados no formato texto, muito utilizado para estruturar informações
 */
