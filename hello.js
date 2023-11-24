899af86a847df5fc1ffe14c75d054e6ecc69ef57
Marlon Brito dos Santos Castor <148072815+Marlon-Brito@users.noreply.github.com>
2023-11-24 10:28:58 -0300

    Utilizando o primeiro pacote instalado

--- hello.js
+++ hello.js
@@ -1,10 +1,14 @@
-console.log('Olá, mundo!');
+const colors = require('colors'); // importando os recursos do pacote colors
+
+console.log('Olá, mundo!'.rainbow);
+console.log(colors.bgYellow('Hello, world!'));
 
 /* Comandos:
     npm -v = mostra a versão do npm
     npm ls = exibe todos os pacotes instalados no projeto
     cls = limpa a tela
     npm init = inicializa o gerenciador instalando o package.json, arquivo que descreve os pacotes e dependências necessários para o projeto
+    npm run <nome do rótulo ou identificador> = executa um script dentro do arquivo package.json
     
     package.json : gerencia os pacotes, documenta o projeto e automaiza alguns serviços
     JSON: modelo de arquivo usado para armazenamento e transmissão de dados no formato texto, muito utilizado para estruturar informações
